3d2650e05caa7c1d594aaa6a50fe72500b83999a
pointfour <lesha.seletskiy@gmail.com>
2019-01-11 10:41:47 -0800

    more functionality more faster

--- page.js
+++ page.js
@@ -1,6 +1,7 @@
 let opened = false;
 let selectedindex = -1;
 let selectedElem;
+let outputs;
 //initializing elements
 const magicelem = elemMaker("div", {
   id: "magic-elem",
@@ -62,9 +63,14 @@ function open() {
   } else {
     magicelem.style.display = "none";
     magicsearch.value = "";
+    renderoutput();
   }
 }
 let searchables = [
+  {
+    name: "",
+    func: () => {}
+  },
   {
     name: "alert",
     func() {
@@ -108,16 +114,116 @@ let searchables = [
         document.designMode = "off";
       }
     }
+  },
+  {
+    name: "dark theme",
+    func() {
+      console.log("night");
+      function rgbToHsl(r, g, b) {
+        (r /= 255), (g /= 255), (b /= 255);
+        var max = Math.max(r, g, b),
+          min = Math.min(r, g, b);
+        var h,
+          s,
+          l = (max + min) / 2;
+
+        if (max == min) {
+          h = s = 0; // achromatic
+        } else {
+          var d = max - min;
+          s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+          switch (max) {
+            case r:
+              h = (g - b) / d + (g < b ? 6 : 0);
+              break;
+            case g:
+              h = (b - r) / d + 2;
+              break;
+            case b:
+              h = (r - g) / d + 4;
+              break;
+          }
+          h /= 6;
+        }
+
+        return [h, s, l];
+      }
+      function backgroundColor(input) {
+        let hsl = rgbToHsl(input[0], input[1], input[2]);
+        return `hsl(${hsl[0]},${hsl[1] * 100}%,${100 - hsl[2] * 100}%)`;
+      }
+      let elems = document.getElementsByTagName("*");
+      for (let i = 0; i < elems.length; i++) {
+        let styles = window.getComputedStyle(elems[i]);
+        console.log(
+          styles.backgroundColor
+            .split("(")[1]
+            .slice(0, -1)
+            .replace(/ /g, "")
+            .split(",")
+        );
+        let newBackgroundColor = backgroundColor(
+          styles.backgroundColor
+            .split("(")[1]
+            .slice(0, -1)
+            .replace(/ /g, "")
+            .split(",")
+        );
+        console.log(newBackgroundColor);
+        elems[i].style.backgroundColor = newBackgroundColor;
+      }
+    }
+  },
+  {
+    name: "toggle blur",
+    func() {
+      if (document.getElementById("magic-blur-style"))
+        deleteElement("magic-blur-style");
+      else
+        document.head.appendChild(
+          elemMaker("style", {
+            id: "magic-blur-style",
+            innerText: `body > *:not(#magic-elem) {
+              filter: blur(20px);
+            }`
+          })
+        );
+    }
   }
 ];
-let outputs;
+searchables.sort((a, b) => {
+  return +(a.name > b.name) - 0.5;
+});
+function superiorSearch(searched, query) {
+  let queryIrl = query.split("");
+  for (let i = 0; i < searched.length && queryIrl.length > 0; i++) {
+    if (searched[i] == queryIrl[0]) {
+      queryIrl.shift();
+    }
+  }
+  return queryIrl.length == 0;
+}
 function renderoutput() {
   let q = magicsearch.value;
+  try {
+    let ret = eval(`${q}`);
+    // console.log(ret, q);
+    searchables[0].name = ret.toString();
+  } catch (e) {
+    searchables[0].name = "";
+  }
   let found = 0;
-  let i = 0;
+  let i = 1;
   outputs = [];
+  if (searchables[0].name) {
+    found++;
+    outputs.push(searchables[0]);
+  }
   while (found < 5 && i < searchables.length) {
-    if (searchables[i].name.indexOf(q) != -1) outputs.push(searchables[i]);
+    if (superiorSearch(searchables[i].name, q)) {
+      outputs.push(searchables[i]);
+      found++;
+    }
     i++;
   }
   magic_output.innerHTML = "";
@@ -138,3 +244,9 @@ function elemMaker(elem, config) {
   for (key in config) element[key] = config[key];
   return element;
 }
+
+function deleteElement(elementId) {
+  let element = document.getElementById(elementId);
+  element.parentNode.removeChild(element);
+}
+renderoutput();
