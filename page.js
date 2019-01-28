34829d550f3d03f9c4ce9fe41f5aa79f7e1cbc36
pointfour <lesha.seletskiy@gmail.com>
2019-01-28 14:48:06 -0800

    fancy modes

--- page.js
+++ page.js
@@ -2,6 +2,7 @@ let opened = false;
 let selectedindex = -1;
 let selectedElem;
 let outputs;
+let mode = 0;
 //initializing elements
 const magicelem = elemMaker("div", {
   id: "magic-elem",
@@ -34,8 +35,8 @@ magicsearch.addEventListener(
     } else if (e.key == "ArrowUp") {
       selectedindex--;
     } else if (e.key == "Enter") {
-      selectedElem.click();
-      open();
+      if (window.getComputedStyle(selectedElem).display == "block")
+        selectedElem.click();
     } else {
       selectedindex = 0;
     }
@@ -54,6 +55,7 @@ function psuedoselect() {
   }
 }
 function open() {
+  mode = 0;
   opened = !opened;
   if (opened) {
     magicelem.style.display = "block";
@@ -68,7 +70,10 @@ function open() {
 let searchables = [
   {
     name: "",
-    func: () => {}
+    func: q => {
+      let ret = eval(`${q}`);
+      return ret.toString();
+    }
   },
   {
     name: "alert",
@@ -188,10 +193,44 @@ let searchables = [
           })
         );
     }
+  },
+  {
+    name: "molar mass",
+    func(q) {
+      let chunks = [];
+      let cur = "";
+      let molarMass = 0;
+      cur = q[0];
+      for (let i = 1; i < q.length; i++) {
+        if (isNaN(parseInt(q[i])) == isNaN(parseInt(q[i - 1]))) cur += q[i];
+        else {
+          chunks.push(cur);
+          cur = q[i];
+        }
+      }
+      if (cur.length > 0) chunks.push(cur);
+      for (let i = 0; i < chunks.length; i += 2) {
+        console.log(elements[chunks[i]]);
+        molarMass += elements[chunks[i]].molarMass * chunks[i + 1];
+      }
+      console.log(chunks);
+      return `${molarMass}`;
+    },
+    modeSwap: true
+  },
+  {
+    name: "js mode",
+    func() {
+      mode = 0;
+    },
+    modeSwap: true
   }
 ];
 searchables.sort((a, b) => {
   return +(a.name > b.name) - 0.5;
+});
+searchables.map((e, i) => {
+  searchables[i].id = i;
 });
 function superiorSearch(searched, query) {
   let queryIrl = query.split("");
@@ -205,9 +244,8 @@ function superiorSearch(searched, query) {
 function renderoutput() {
   let q = magicsearch.value;
   try {
-    let ret = eval(`${q}`);
-    // console.log(ret, q);
-    searchables[0].name = ret.toString();
+    console.log(mode);
+    searchables[0].name = searchables[mode].func(q);
   } catch (e) {
     searchables[0].name = "";
   }
@@ -231,11 +269,29 @@ function renderoutput() {
     result.classList.add("searchterm");
     result.classList.add("magicthingy");
     result.innerText = elem.name;
-    result.addEventListener("click", e => {
-      elem.func();
-    });
+    if (elem.id == 0) {
+      result.addEventListener("click", e => {
+        const el = elemMaker("textarea", {
+          value: result.innerText
+        });
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand("copy");
+        document.body.removeChild(el);
+        magicsearch.focus();
+        magicsearch.value = null;
+      });
+    } else
+      result.addEventListener("click", e => {
+        if (elem.modeSwap) {
+          mode = elem.id;
+          magicsearch.value = null;
+        } else open();
+        elem.func();
+      });
     magic_output.appendChild(result);
   });
+  selectedElem = null;
   psuedoselect();
 }
 
@@ -251,33 +307,85 @@ function deleteElement(elementId) {
 }
 renderoutput();
 //utils for use
-const C = {
-  s(query) {
-    let ret = [];
-    console.log("search");
-    for (let i = 0; i < C.elements.length; i++) {
-      console.log(C.elements[i]);
-      for (key in C.elements[i]) {
-        if (
-          C.elements[i][key]
-            .toString()
-            .toLowerCase()
-            .includes(query.toLowerCase())
-        ) {
-          ret.push(C.elements[i]);
-          break;
-        }
-      }
-      console.log(ret);
-      return ret;
-    }
+const elements = {
+  h: {
+    molarMass: 1.008,
+    name: "hydrogen",
+    number: 1
   },
-  elements: [
-    {
-      symbol: "H",
-      name: "Hydrogen",
-      molarMass: 1.008,
-      number: 1
-    }
-  ]
+  he: {
+    molarMass: 4.003,
+    name: "helium",
+    number: 2
+  },
+  li: {
+    molarMass: 6.94,
+    name: "lithium",
+    number: 3
+  },
+  be: {
+    molarMass: 9.012,
+    name: "beryllium",
+    number: 4
+  },
+  b: {
+    molarMass: 10.81,
+    name: "boron",
+    number: 5
+  },
+  c: {
+    molarMass: 12.011,
+    name: "carbon",
+    number: 6
+  },
+  n: {
+    molarMass: 14.007,
+    name: "nitrogen",
+    number: 7
+  },
+  o: {
+    molarMass: 15.999,
+    name: "oxygen",
+    number: 8
+  },
+  f: {
+    molarMass: 18.998,
+    name: "fluorine",
+    number: 9
+  },
+  ne: {
+    molarMass: 20.18,
+    name: "neon",
+    number: 10
+  },
+  na: {
+    molarMass: 22.99,
+    name: "sodium",
+    number: 11
+  },
+  mg: {
+    molarMass: 24.305,
+    name: "magnesium",
+    number: 12
+  },
+  al: {
+    molarMass: 26.982,
+    name: "aluminium",
+    number: 13
+  },
+  si: {
+    molarMass: 28.085,
+    name: "silicon",
+    number: 14
+  },
+  p: {
+    molarMass: 30.974,
+    name: "phosphorus",
+    number: 15
+  }, //out of orders
+  fe: {
+    molarMass: 55.845,
+    name: "iron",
+    number: 26
+  }
 };
